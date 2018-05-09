4a2aa0db30b30e4e076c2f8af226c2e285e67788
Meghan Gorman <fluteloops27@gmail.com>
2018-05-08 20:54:28 -0700

    now outputs to file instead of cout

--- ray-tracing-weekend/main.cpp
+++ ray-tracing-weekend/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
     int nx = 200;
     int ny = 100;
-    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+
+    std::ofstream ppm;
+    ppm.open("image.ppm");
+    ppm << "P3\n" << nx << " " << ny << "\n255\n";
     for (int i = ny - 1; i >= 0; i--) {
         for (int j = 0; j < nx; j++) {
             float r = float(j) / float(nx);
@@ -12,7 +16,8 @@ int main() {
             int ir = int(255.99*r);
             int ig = int(255.99*g);
             int ib = int(255.99*b);
-            std::cout << ir << " " << ig << " " << ib << "\n";
+            ppm << ir << " " << ig << " " << ib << "\n";
         }
     }
+    ppm.close();
 }
