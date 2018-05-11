eb857f52f22abbb2e34c9d650fa3e955efeb22c5
Meghan Gorman <fluteloops27@gmail.com>
2018-05-10 21:18:28 -0700

    Added red sphere

--- ray-tracing-weekend/vec3.h
+++ ray-tracing-weekend/vec3.h
@@ -35,8 +35,18 @@ public:
     inline void make_unit_vector();
 };
 
+inline std::istream& operator>>(std::istream &is, vec3 &t) {
+    is >> t.e[0] >> t.e[1] >> t.e[2];
+    return is;
+}
+
+inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
+    os << t.e[0] << " " << t.e[1] << " " << t.e[2];
+    return os;
+}
+
 inline void vec3::make_unit_vector() {
-    float k = 1.0 / sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+    float k = float(1.0) / sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
     e[0] *= k; e[1] *= k; e[2] *= k;
 }
 
@@ -66,6 +76,16 @@ inline vec3 operator/(vec3 v, float t) {
 
 inline vec3 operator*(const vec3 &v, float t) {
     return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+}
+
+inline float dot(const vec3 &v1, const vec3 &v2) {
+    return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
+}
+
+inline vec3 cross(const vec3 &v1, const vec3 &v2) {
+    return vec3((v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1]),
+        (-(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0])),
+        (v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]));
 }
 
 
@@ -105,7 +125,7 @@ inline vec3& vec3::operator*=(const float t) {
 }
 
 inline vec3& vec3::operator/=(const float t) {
-    float k = 1.0 / t;
+    float k = float(1.0 / t);
 
     e[0] *= k;
     e[1] *= k;
