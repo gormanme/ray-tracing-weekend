82cfe825de4914ff230ce80524b84ceb1a2fa4f5
Meghan Gorman <fluteloops27@gmail.com>
2018-05-12 18:50:17 -0700

    added hitable abstract class and another sphere

--- ray-tracing-weekend/main.cpp
+++ ray-tracing-weekend/main.cpp
@@ -1,30 +1,20 @@
 #include <iostream>
 #include <fstream>
-#include "ray.h"
+#include <cfloat>
+#include "sphere.h"
+#include "hitable_list.h"
 
-float hit_sphere(const vec3& center, float radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    float a = dot(r.direction(), r.direction());
-    float b = 2.0f * dot(oc, r.direction());
-    float c = dot(oc, oc) - radius * radius;
-    float discriminant = b * b - 4 * a*c;
-    if (discriminant < 0) {
-        return -1.0f;
+
+vec3 color(const ray& r, hitable *world) {
+    hit_record rec = {};
+    if (world->hit(r, 0.0f, FLT_MAX, rec)) {
+        return 0.5f*vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
     }
     else {
-        return (-b - sqrt(discriminant)) / (2.0f * a);
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5f * (unit_direction.y() + 1.0f);
+        return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
     }
-}
-
-vec3 color(const ray& r) {
-    float t = hit_sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f, r);
-    if (t > 0.0f) {
-        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0.0f, 0.0f, -1.0f));
-        return 0.5f * vec3(N.x() + 1.0f, N.y() + 1.0f, N.z() + 1.0f);
-    }
-    vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5f * (unit_direction.y() + 1.0f);
-    return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
 }
 
 int main() {
@@ -37,12 +27,18 @@ int main() {
     vec3 horizontal(4.0f, 0.0f, 0.0f);
     vec3 vertical(0.0f, 2.0f, 0.0f);
     vec3 origin(0.0f, 0.0f, 0.0f);
+    hitable *list[2] = {};
+    list[0] = new sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f);
+    list[1] = new sphere(vec3(0.0f, -100.5f, -1.0f), 100);
+    hitable *world = new hitable_list(list, 2);
     for (int i = ny - 1; i >= 0; i--) {
         for (int j = 0; j < nx; j++) {
             float u = float(j) / float(nx);
             float v = float(i) / float(ny);
             ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            vec3 col = color(r);
+            
+            vec3 p = r.point_at_parameter(2.0f);
+            vec3 col = color(r, world);
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
