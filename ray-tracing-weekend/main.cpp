dc15e2d83b1d366b61c03464af051766f8a2a000
Meghan Gorman <fluteloops27@gmail.com>
2018-05-12 19:37:48 -0700

    Added antialiasing

--- ray-tracing-weekend/main.cpp
+++ ray-tracing-weekend/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <cfloat>
+#include <stdlib.h>
 #include "sphere.h"
 #include "hitable_list.h"
+#include "camera.h"
 
 
 vec3 color(const ray& r, hitable *world) {
@@ -20,25 +22,27 @@ vec3 color(const ray& r, hitable *world) {
 int main() {
     int nx = 200;
     int ny = 100;
+    int ns = 100;
     std::ofstream ppm;
     ppm.open("image.ppm");
     ppm << "P3\n" << nx << " " << ny << "\n255\n";
-    vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
-    vec3 horizontal(4.0f, 0.0f, 0.0f);
-    vec3 vertical(0.0f, 2.0f, 0.0f);
-    vec3 origin(0.0f, 0.0f, 0.0f);
+
     hitable *list[2] = {};
     list[0] = new sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f);
     list[1] = new sphere(vec3(0.0f, -100.5f, -1.0f), 100);
     hitable *world = new hitable_list(list, 2);
+    camera cam = {};
     for (int i = ny - 1; i >= 0; i--) {
         for (int j = 0; j < nx; j++) {
-            float u = float(j) / float(nx);
-            float v = float(i) / float(ny);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            
-            vec3 p = r.point_at_parameter(2.0f);
-            vec3 col = color(r, world);
+            vec3 col(0.0f, 0.0f, 0.0f);
+            for (int s = 0; s < ns; s++) {
+                float u = float(j + rand()/(float)RAND_MAX) / float(nx);
+                float v = float(i + rand()/(float)RAND_MAX) / float(ny);
+                ray r = cam.get_ray(u, v);
+                vec3 p = r.point_at_parameter(2.0f);
+                col += color(r, world);
+            }
+            col /= float(ns);
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
