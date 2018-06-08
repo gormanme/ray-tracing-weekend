e93dfce180e57e37ee72066cfe21a983141e9c3b
Meghan Gorman <fluteloops27@gmail.com>
2018-06-07 20:18:29 -0700

    Metal material (chapter 8)

--- ray-tracing-weekend/main.cpp
+++ ray-tracing-weekend/main.cpp
@@ -5,20 +5,19 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "camera.h"
+#include "material.h"
 
-vec3 random_in_unit_sphere() {
-    vec3 p = {};
-    do {
-        p = 2.0f * vec3(rand()/(float)RAND_MAX, rand()/(float)RAND_MAX, rand()/(float)RAND_MAX) - vec3(1.0f, 1.0f, 1.0f);
-    } while (p.squared_length() >= 1.0f);
-    return p;
-}
-
-vec3 color(const ray& r, hitable *world) {
-    hit_record rec = {};
+vec3 color(const ray& r, hitable *world, int depth) {
+    hit_record rec;
     if (world->hit(r, 0.001f, FLT_MAX, rec)) {
-        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
-        return 0.5f*color(ray(rec.p, target - rec.p), world);
+        ray scattered = {};
+        vec3 attenuation = {};
+        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+            return attenuation * color(scattered, world, depth + 1);
+        }
+        else {
+            return vec3(0.0f, 0.0f, 0.0f);
+        }
     }
     else {
         vec3 unit_direction = unit_vector(r.direction());
@@ -35,10 +34,12 @@ int main() {
     ppm.open("image.ppm");
     ppm << "P3\n" << nx << " " << ny << "\n255\n";
 
-    hitable *list[2] = {};
-    list[0] = new sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f);
-    list[1] = new sphere(vec3(0.0f, -100.5f, -1.0f), 100);
-    hitable *world = new hitable_list(list, 2);
+    hitable *list[4] = {};
+    list[0] = new sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f, new lambertian(vec3(0.8f, 0.3f, 0.3f)));
+    list[1] = new sphere(vec3(0.0f, -100.5f, -1.0f), 100.0f, new lambertian(vec3(0.8f, 0.8f, 0.0f)));
+    list[2] = new sphere(vec3(1.0f, 0.0f, -1.0f), 0.5f, new metal(vec3(0.8f, 0.6f, 0.2f), 1.0f));
+    list[3] = new sphere(vec3(-1.0f, 0.0f, -1.0f), 0.5f, new metal(vec3(0.8f, 0.8f, 0.8f), 0.3f));
+    hitable *world = new hitable_list(list, 4);
     camera cam = {};
     for (int i = ny - 1; i >= 0; i--) {
         for (int j = 0; j < nx; j++) {
@@ -48,7 +49,7 @@ int main() {
                 float v = float(i + rand()/(float)RAND_MAX) / float(ny);
                 ray r = cam.get_ray(u, v);
                 vec3 p = r.point_at_parameter(2.0f);
-                col += color(r, world);
+                col += color(r, world, 0);
             }
             col /= float(ns);
             col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
