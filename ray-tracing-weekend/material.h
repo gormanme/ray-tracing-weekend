81856234f1bda5095023acdf0786fdd1f12b97de
Meghan Gorman <fluteloops27@gmail.com>
2018-06-09 17:20:32 -0700

    Dielectric sphere (chapter 9)

--- ray-tracing-weekend/material.h
+++ ray-tracing-weekend/material.h
@@ -18,8 +18,27 @@ vec3 random_in_unit_sphere() {
     return p;
 }
 
+bool refract(const vec3& v, const vec3& n, float ni_over_nt, vec3& refracted) {
+    vec3 uv = unit_vector(v);
+    float dt = dot(uv, n);
+    float discriminant = 1.0f - ni_over_nt * ni_over_nt * (1 - dt * dt);
+    if (discriminant > 0) {
+        refracted = ni_over_nt * (uv - n * dt) - n * sqrt(discriminant);
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2 * dot(v, n)*n;
+}
+
+float schlick(float cosine, float ref_idx) {
+    float r0 = (1 - ref_idx) / (1 + ref_idx);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * pow((1 - cosine), 5);
 }
 
 class lambertian : public material {
@@ -40,10 +59,50 @@ public:
     metal(const vec3& a, float f) : albedo(a) { if (f < 1) fuzz = f; else fuzz = 1.0f; }
     virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered = ray(rec.p, reflected + fuzz*random_in_unit_sphere());
+        scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
         attenuation = albedo;
         return (dot(scattered.direction(), rec.normal) > 0);
     }
     vec3 albedo = {};
-    float fuzz;
+    float fuzz = 0.0f;
+};
+
+class dielectric : public material {
+public:
+    dielectric(float ri) : ref_idx(ri) {}
+    virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const {
+        vec3 outward_normal = {};
+        vec3 reflected = reflect(r_in.direction(), rec.normal);
+        float ni_over_nt = 0.0f;
+        attenuation = vec3(1.0f, 1.0f, 1.0f);
+        vec3 refracted = {};
+        float reflect_prob = 0.0f;
+        float cosine = 0.0f;
+        if (dot(r_in.direction(), rec.normal) > 0) {
+            outward_normal = -1.0f * rec.normal;
+            ni_over_nt = ref_idx;
+            cosine = ref_idx * dot(r_in.direction(), rec.normal) / r_in.direction().length();
+        }
+        else {
+            outward_normal = rec.normal;
+            ni_over_nt = 1.0f / ref_idx;
+            cosine = -dot(r_in.direction(), rec.normal) / r_in.direction().length();
+        }
+        if (refract(r_in.direction(), outward_normal, ni_over_nt, refracted)) {
+            reflect_prob = schlick(cosine, ref_idx);
+        }
+        else {
+            scattered = ray(rec.p, reflected);
+            reflect_prob = 1.0f;
+        }
+        if (rand() / (float)RAND_MAX < reflect_prob){
+            scattered = ray(rec.p, reflected);
+        }
+        else {
+            scattered = ray(rec.p, refracted);
+        }
+        return true;
+    }
+
+    float ref_idx;
 };
