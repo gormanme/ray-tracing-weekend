115a022896eaca66b20a6e825c678e07f62d93c7
Meghan Gorman <fluteloops27@gmail.com>
2018-06-10 16:22:49 -0700

    Positional camera (chapter 10)

--- ray-tracing-weekend/camera.h
+++ ray-tracing-weekend/camera.h
@@ -1,13 +1,22 @@
 #pragma once
 #include "ray.h"
 
+const float PI = 3.1415f;
+
 class camera {
 public:
-    camera() {
-        lower_left_corner = vec3(-2.0f, -1.0f, -1.0f);
-        horizontal = vec3(4.0f, 0.0f, 0.0f);
-        vertical = vec3(0.0f, 2.0f, 0.0f);
-        origin = vec3(0.0f, 0.0f, 0.0f);
+    camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect) { //vfov is the top to bottom in degrees (vertical field of view)
+        vec3 u, v, w = {};
+        float theta = vfov * PI / 180.0f;
+        float half_height = tan(theta / 2);
+        float half_width = aspect * half_height;
+        origin = lookfrom;
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
+        lower_left_corner = origin - half_width * u - half_height * v - w;
+        horizontal = 2 * half_width*u;
+        vertical = 2 * half_height*v;
     }
     ray get_ray(float u, float v) {
         return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
